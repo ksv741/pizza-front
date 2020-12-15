ef0be95b767b23a6c9f49c940bcc72261dc426bd
ksv741 <ksv741@mail.ru>
2020-12-15 21:08:23 +0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+    <h1>
+      Pizza Test
+    </h1>
+);
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
